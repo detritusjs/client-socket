cee80ecc98cd4005afaa9cec06ba904dcb3bd8f1
Dan <dansoul.cake@gmail.com>
2019-09-12 22:43:31 -0400

    utils version bump

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const Package = Object.freeze({
   URL: 'https://github.com/detritusjs/client-socket',
-  VERSION: '0.2.24',
+  VERSION: '0.2.25',
 });
 
 function normalize(object: {[key: string]: any}) {
